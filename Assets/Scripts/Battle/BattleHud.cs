3f0120f9860ac22c9dfd8afd5af52d62fd4a255f
Emb3rlight <carl.tenko@gmail.com>
2020-12-18 17:10:54 +0000

    test

--- Assets/Scripts/Battle/BattleHud.cs
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -4,26 +4,63 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BattleHud : MonoBehaviour
 {
-   
-    [SerializeField] Text Player1NameText; 
+
+
+ [SerializeField] Text Player1NameText; 
     [SerializeField] Text Enemy1NameText; 
     [SerializeField] Text Enemy2NameText; 
     [SerializeField] Text levelText; 
     [SerializeField] Text ATKText;
 
-
-
-
-    public void SetData(Char Char)
+    public void SetData1(Char Char)
     {
 
         //test 123
         Player1NameText.text = Char.Base.Name;
-        Enemy1NameText.text = Char.Base.Name;
-        Enemy2NameText.text = Char.Base.Name;
         levelText.text = "Level " + Char.Level;
-        ATKText.text = "ATK " + Char.Attack;
+        
     }
 
 
+public void SetData2(Char Char)
+    {
+
+        //test 123
+        Enemy1NameText.text = Char.Base.Name;
+        
+
+      
+    }
+
+
+public void SetData3(Char Char)
+    {
+
+        //test 123
+        Enemy2NameText.text = Char.Base.Name;
+        ATKText.text = "ATK " + Char.Strength;
+      
+
+    }
+
+  
+
+  
+  //  public void SetData(Char playerUnit1,Char enemyUnit1, Char enemyUnit2)
+  /*  {
+
+        
+        Player1NameText.text = playerUnit1.Base.Name;
+        Enemy1NameText.text = enemyUnit1.Base.Name;
+        Enemy2NameText.text = enemyUnit2.Base.Name;
+
+        levelText.text = "Level " + enemyUnit2.Base.Magic;
+        
+        ATKText.text = "ATK " + playerUnit1.Base.Strength;
+        
+        
+
+    }
+
+*/
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
- 
     [SerializeField] BattleUnit playerUnit1; 
     [SerializeField] BattleHud playerHud; 
     [SerializeField] BattleUnit enemyUnit1; 
     [SerializeField] BattleUnit enemyUnit2; 
+
+    [SerializeField] BattleHud playerHud123456789;
 
 private void Start ()
 {
@@ -20,13 +21,20 @@ private void SetupBattle ()
     playerUnit1.Setup();
     enemyUnit1.Setup();
     enemyUnit2.Setup();
-    
-    playerHud.SetData(playerUnit1.Char);
-    playerHud.SetData(enemyUnit1.Char);
-    playerHud.SetData(enemyUnit2.Char);
+
+    playerHud.SetData1(playerUnit1.Char);
+    playerHud.SetData2(enemyUnit1.Char);
+    playerHud.SetData3(enemyUnit2.Char);
+
 
     Debug.Log($"str = {playerUnit1.Char.Strength}");
     Debug.Log($"magic = {playerUnit1.Char.Magic}");
+
+    Debug.Log($"str = {enemyUnit1.Char.Strength}");
+    Debug.Log($"magic = {enemyUnit1.Char.Magic}");
+
+    Debug.Log($"str = {enemyUnit2.Char.Strength}");
+    Debug.Log($"magic = {enemyUnit2.Char.Magic}");
 
 }
 
